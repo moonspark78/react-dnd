aec7f8e35cee787af8f7ea20d86b533a68944c43
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-01-29 18:42:57 +0100

    ✨creation du tableau

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd"
+import "./App.css"
+
+const listItems = [
+	{
+		id: "1",
+		name: "Apprendre L'Espagnol"
+	},
+	{
+		id: "2",
+		name: "Sport"
+	},
+	{
+		id: "3",
+		name: "Netflix & Chill"
+	},
+	{
+		id: "4",
+		name: "Repos"
+	}
+]
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+	padding: 10,
+	margin: `0 50px 15px 50px`,
+	background: isDragging ? "#4a2975" : "white",
+	color: isDragging ? "white" : "black",
+	border: `1px solid black`,
+	fontSize: `20px`,
+	borderRadius: `5px`,
+
+	...draggableStyle
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	return (
+		<div className="App">
+			<h1>Drag and Drop</h1>
+			<DragDropContext onDragEnd={onDragEnd}>
+				
+			</DragDropContext>
+		</div>
+	)
 }
 
-export default App;
+export default App
